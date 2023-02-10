88b32046b5c7b870f3c6d8d1cf33defc2f845993
Christian v Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2023-02-10 19:27:16 +0100

    Login, register, signout functions

--- frontend/tfginder/src/app/services/auth.service.ts
+++ frontend/tfginder/src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  login(email: string, password: string){
+    try {
+      const userCredential = signInWithEmailAndPassword(this.auth, email, password);
+      return userCredential;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  logout(){ return signOut(this.auth);}
+
+  register(email: string, password: string){
+    try {
+      const userCredential = createUserWithEmailAndPassword(this.auth, email, password);
+      return userCredential;
+    } catch (error) {
+      console.log(error);
+      return null;  
+    }
+  }
 }
