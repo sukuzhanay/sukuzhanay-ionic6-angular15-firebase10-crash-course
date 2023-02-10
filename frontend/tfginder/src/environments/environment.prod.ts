11ab27fa0928011a30c84c9cf5dbeb7a87ee0faf
Christian v Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2023-02-10 15:56:09 +0100

    Added firebase backend features for Tfginder app

--- frontend/tfginder/src/environments/environment.prod.ts
+++ frontend/tfginder/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-2c3db',
+    appId: '1:979370109529:web:980b72cd7d21307eb31f48',
+    storageBucket: 'ionic-2c3db.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyBRnUqiHbAwB2S6IRgykI_bxrGbBWvCC2I',
+    authDomain: 'ionic-2c3db.firebaseapp.com',
+    messagingSenderId: '979370109529',
+    measurementId: 'G-R3SHS3QGND',
+  },
   production: true
 };
--- frontend/tfginder/src/environments/environment.ts
+++ frontend/tfginder/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionic-2c3db',
+    appId: '1:979370109529:web:980b72cd7d21307eb31f48',
+    storageBucket: 'ionic-2c3db.appspot.com',
+    locationId: 'europe-west3',
+    apiKey: 'AIzaSyBRnUqiHbAwB2S6IRgykI_bxrGbBWvCC2I',
+    authDomain: 'ionic-2c3db.firebaseapp.com',
+    messagingSenderId: '979370109529',
+    measurementId: 'G-R3SHS3QGND',
+  },
   production: false
 };
 
