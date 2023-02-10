edb5cd5e02e6b2c99558c0565f1b6fb358139310
Christian v Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2023-02-10 18:36:18 +0100

    Created routes for app, app-routing.module.ts

--- frontend/tfginder/src/app/app-routing.module.ts
+++ frontend/tfginder/src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
+    path: '**',
+    redirectTo: '',
     pathMatch: 'full'
   },
   {
-    path: 'login',
+    path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
 ];
