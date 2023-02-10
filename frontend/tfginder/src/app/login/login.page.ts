96c1baf86d5b514c41a0b0ea7bda8adb228734a6
Christian v Sucuzhanay Arevalo <sukuzhanay@gmail.com>
2023-02-10 19:57:35 +0100

    import libs, def constructor, impl ngOnit, valid form

--- frontend/tfginder/src/app/login/login.page.ts
+++ frontend/tfginder/src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Validators, FormGroup, FormBuilder, FormControl}from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+  
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController
+  ) { }
+  
+  get password() { return this.validations_form.get('password'); }
+  get email() { return this.validations_form.get('email'); }
 
   ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
   }
 
 }
